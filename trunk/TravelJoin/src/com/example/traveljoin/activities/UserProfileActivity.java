33faf59c0b317a324ba69cb73c965f4847cf2823
hernan.pieroni@gmail.com <hernan.pieroni@gmail.com@704b2364-5aca-72fd-fb40-33b75dc59cb9>
2014-09-01 03:15:10 +0000

    Primera aproximación a la pantalla de perfil de usuario realizada con Tabs.

--- trunk/TravelJoin/src/com/example/traveljoin/activities/UserProfileActivity.java
+++ trunk/TravelJoin/src/com/example/traveljoin/activities/UserProfileActivity.java
@@ -9,121 +9,216 @@ import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.ProfilePictureView;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class UserProfileActivity extends FragmentActivity {
+public class UserProfileActivity extends FragmentActivity implements ActionBar.TabListener {
 
-	private ProfilePictureView profilePictureView;
-	private TextView userNameView;
-	private static final int REAUTH_ACTIVITY_CODE = 100;
+    AppSectionsPagerAdapter appSectionsPagerAdapter;
+    ViewPager viewPager;
+    ActionBar actionBar;
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
 
-	private UiLifecycleHelper uiHelper;
-	private Session.StatusCallback callback = new Session.StatusCallback() {
-		@Override
-		public void call(final Session session, final SessionState state,
-				final Exception exception) {
-			onSessionStateChange(session, state, exception);
-		}
-	};	
+        appSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
+        actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(appSectionsPagerAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("General")
+                        .setTabListener(this));
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Grupos")
+                        .setTabListener(this));
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("POIs")
+                        .setTabListener(this));
+        actionBar.addTab(
+                actionBar.newTab()
+                        .setText("Circuitos")
+                        .setTabListener(this));
+    }
 
-		uiHelper = new UiLifecycleHelper(this, callback);
-		uiHelper.onCreate(savedInstanceState);
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
 
-		setContentView(R.layout.activity_user_profile);
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
 
-		// Find the user's profile picture custom view
-		profilePictureView = (ProfilePictureView) findViewById(R.id.selection_profile_pic);
-		profilePictureView.setCropped(true);
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
 
-		// Find the user's name view
-		userNameView = (TextView) findViewById(R.id.selection_user_name);
-		// Check for an open session
-		
-	    Session session = Session.getActiveSession();
-	    if (session != null && session.isOpened()) {
-	        // Get the user's data
-	        makeMeRequest(session);
-	    }
-	    
-	}
+    public static class AppSectionsPagerAdapter extends FragmentPagerAdapter {
+    	
+    	public static final int USER_INFORMATION_TAB = 0;
+    	public static final int USER_GROUPS_TAB = 1;
+    	public static final int USER_POIS_TAB = 2;
+    	public static final int USER_CIRCUITS_TAB = 3;
+    	
+        public AppSectionsPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
 
-	private void onSessionStateChange(Session session, SessionState state,
-			Exception exception) {
-		// Only make changes if the activity is visible
-		if (session != null && session.isOpened()) {
-			makeMeRequest(session);
-		} else {
-			Intent intent = new Intent(this, MainActivity.class);
-			startActivity(intent);
-		}
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment;
+        	switch (position) {
+                case USER_INFORMATION_TAB:
+                	fragment = new UserInformationFragment();
+                	break;
+                case USER_GROUPS_TAB:
+                	fragment = new UserInformationFragment();
+                	break;
+                case USER_POIS_TAB:
+                	fragment = new UserInformationFragment();
+                	break;
+                case USER_CIRCUITS_TAB:
+                	fragment = new UserInformationFragment();
+                	break;
+                default:
+                    fragment = new UserInformationFragment();
+                    break;
+            }
+            return fragment;
+        }
 
-	}
+        @Override
+        public int getCount() {
+            return 4;
+        }
+    }
 
-	private void makeMeRequest(final Session session) {
-		// Make an API call to get user data and define a
-		// new callback to handle the response.
-		Request request = Request.newMeRequest(session,
-				new Request.GraphUserCallback() {
-					@Override
-					public void onCompleted(GraphUser user, Response response) {
-						// If the response is successful
-						if (session == Session.getActiveSession()) {
-							if (user != null) {
-								// Set the id for the ProfilePictureView
-								// view that in turn displays the profile
-								// picture.
-								profilePictureView.setProfileId(user.getId());
-								// Set the Textview's text to the user's name.
-								userNameView.setText(user.getName());
-								//user.getId();
-							}
-						}
-						if (response.getError() != null) {
-							// Handle errors, will do so later.
-						}
-						
-					}
-				});
-		request.executeAsync();
-	}
+    public static class UserInformationFragment extends Fragment {
+    	
+    	private ProfilePictureView profilePictureView;
+    	private TextView userNameView;
+    	private static final int REAUTH_ACTIVITY_CODE = 100;
+    	
+    	private UiLifecycleHelper uiHelper;
+    	private Session.StatusCallback callback = new Session.StatusCallback() {
+    		@Override
+    		public void call(final Session session, final SessionState state,
+    				final Exception exception) {
+    			onSessionStateChange(session, state, exception);
+    		}
+    	};
+    	
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+          	
+        	View view = inflater.inflate(R.layout.fragment_user_information, container, false);  
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		uiHelper.onResume();
-	}
+    		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+    		uiHelper.onCreate(savedInstanceState);
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		uiHelper.onPause();
-	}
+    		profilePictureView = (ProfilePictureView) view.findViewById(R.id.selection_profile_pic);
+    		profilePictureView.setCropped(true);
+    		userNameView = (TextView) view.findViewById(R.id.selection_user_name);
+    		
+    	    Session session = Session.getActiveSession();
+    	    if (session != null && session.isOpened()) {
+    	        makeMeRequest(session);
+    	    }
+    	    
+    	    return view;
+    	    
+    	}
 
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-	    if (requestCode == REAUTH_ACTIVITY_CODE) {
-	        uiHelper.onActivityResult(requestCode, resultCode, data);
-	    }
-	}
+    	private void onSessionStateChange(Session session, SessionState state,
+    			Exception exception) {
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		uiHelper.onDestroy();
-	}
+    		if (session != null && session.isOpened()) {
+    			makeMeRequest(session);
+    		} else {
+    			Intent intent = new Intent(getActivity(), MainActivity.class);
+    			startActivity(intent);
+    		}
 
-	@Override
-	protected void onSaveInstanceState(Bundle bundle) {
-		super.onSaveInstanceState(bundle);
-		uiHelper.onSaveInstanceState(bundle);
-	}
+    	}
 
+    	private void makeMeRequest(final Session session) {
+    		
+    		Request request = Request.newMeRequest(session,
+    				new Request.GraphUserCallback() {
+    					@Override
+    					public void onCompleted(GraphUser user, Response response) {
+    						// If the response is successful
+    						if (session == Session.getActiveSession()) {
+    							if (user != null) {
+    								profilePictureView.setProfileId(user.getId());
+    								userNameView.setText(user.getName());
+    							}
+    						}
+    						if (response.getError() != null) {
+    							//TODO: Handle errors, will do so later.
+    						}
+    						
+    					}
+    				});
+    		request.executeAsync();
+    	}
+
+    	@Override
+    	public void onResume() {
+    		super.onResume();
+    		uiHelper.onResume();
+    	}
+
+    	@Override
+    	public void onPause() {
+    		super.onPause();
+    		uiHelper.onPause();
+    	}
+
+    	@Override
+    	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    		super.onActivityResult(requestCode, resultCode, data);
+    	    if (requestCode == REAUTH_ACTIVITY_CODE) {
+    	        uiHelper.onActivityResult(requestCode, resultCode, data);
+    	    }
+    	}
+
+    	@Override
+    	public void onDestroy() {
+    		super.onDestroy();
+    		uiHelper.onDestroy();
+    	}
+
+    	@Override
+    	public void onSaveInstanceState(Bundle bundle) {
+    		super.onSaveInstanceState(bundle);
+    		uiHelper.onSaveInstanceState(bundle);
+    	}
+
+    }
 }
