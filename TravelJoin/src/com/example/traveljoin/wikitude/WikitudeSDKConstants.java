4d8f66c1c0baec2af96db7379fce80cd634b3ad4
pablo.biscione@gmail.com <pablo.biscione@gmail.com@704b2364-5aca-72fd-fb40-33b75dc59cb9>
2014-09-22 00:54:41 +0000

    Realidad Aumentada: Primera iteracion con pois random. Ejecutando actividad de wikitude ya desde nuestro menu.

---
+++ TravelJoin/src/com/example/traveljoin/wikitude/WikitudeSDKConstants.java
@@ -0,0 +1,11 @@
+package com.example.traveljoin.wikitude;
+
+public class WikitudeSDKConstants {
+	
+
+	/**
+	 * note that this key is only valid for this sample app, if you need one for your own application please visit www.wikitude.com (SDK Store)
+	 */
+	protected static final String WIKITUDE_SDK_KEY = "anuBULWBofTSivUwsjn8lySHF6Hhs+sl5QjENgLXWKBe5AdeqHzZO6piTqseS7I72FA58+WfnEhfYle6cDFEIsSAA7xa6L3KY/0T4x+ljjGhiJqjHmQntNbBGWpWO5kZrX4R9gDsRj9dCRl8egDs0qRzuwY5VosriuHd0mNY5PZTYWx0ZWRfXzW9EhW7RaO3FKbilvFkx3lvBb9rsWOmZV+3nOzPu0dAlzQzTWYUtfm6jlh8MpcllI4U6i7Hpm/Pkbyg8r/rE0sGEBWouud93/Mp2hvWw6nmhogjyFLaDjOh7hrnmRBlWHp7MUVHblYUlF9Bp+erSJx3MiD5Uy0KjnXi7kJldgPb+KpNqyqeDUe7JWpSQ/V+09FvcY3V17jbLYNjVKBATtILQeTiSetIrGjQObeNZbeonow2FU3A+qMBPnECPlYe/aeB8hpOeU3R2xUttFu/kNlrK60hnJaN+qLJbKiqt82GheC6hKpHDCKYwZV8N9QvsXjHl2ps2KSmj99MajNCZTgGLj4/AvP0A3zjR5J8cqYuS1Qt5QqqVxgwbPYMtoa4G850dwNmykUoD6HZXp0MuWiM16OAaxRL5jeDiQNt91nqGzxlN0Q8PW6DK/qKwd5F8tNAnyO7YjXDYsAUT9BPM6FzDiiyDezPR+2GCsvNgsCWcSgmq0f1M04=";
+
+}
