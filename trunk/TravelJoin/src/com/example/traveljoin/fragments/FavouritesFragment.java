3a20ae62c5430e8552acb11e8fe4e8468677f3b6
hernan.pieroni@gmail.com <hernan.pieroni@gmail.com@704b2364-5aca-72fd-fb40-33b75dc59cb9>
2014-09-22 22:17:24 +0000

    Perfil de usuario: La lista de puntos de interes ahora se cargan desde el servidor con todos aquellos puntos de interes creados por el usuario

--- trunk/TravelJoin/src/com/example/traveljoin/fragments/FavouritesFragment.java
+++ trunk/TravelJoin/src/com/example/traveljoin/fragments/FavouritesFragment.java
@@ -1,7 +1,15 @@
 package com.example.traveljoin.fragments;
 
+import java.text.ParseException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -16,15 +24,21 @@ import android.widget.ExpandableListView.ExpandableListContextMenuInfo;
 
 import com.example.traveljoin.R;
 import com.example.traveljoin.adapters.FavouritesExpandableListAdapter;
+import com.example.traveljoin.auxiliaries.GlobalContext;
+import com.example.traveljoin.models.ApiInterface;
+import com.example.traveljoin.models.ApiResult;
 import com.example.traveljoin.models.GeneralItem;
 import com.example.traveljoin.models.GroupFavouriteItems;
 import com.example.traveljoin.models.Poi;
 import com.example.traveljoin.models.Tour;
+import com.example.traveljoin.models.User;
 
 public class FavouritesFragment extends Fragment {
 
 	private final static int POIS_GROUP_KEY = 0;
 	private final static int TOURS_GROUP_KEY = 1;
+	private GroupFavouriteItems poisGroup;
+	private GroupFavouriteItems toursGroup;
 	private SparseArray<GroupFavouriteItems> groups = new SparseArray<GroupFavouriteItems>();
 
 	@Override
@@ -35,7 +49,15 @@ public class FavouritesFragment extends Fragment {
 		View view = inflater.inflate(R.layout.fragment_user_favourites,
 				container, false);
 
+		poisGroup = new GroupFavouriteItems(
+				getString(R.string.favourite_pois_group),
+				R.drawable.ic_action_place);
+		toursGroup = new GroupFavouriteItems(
+				getString(R.string.favourite_tours_group),
+				R.drawable.ic_action_split);
+
 		createItemsGroups();
+
 		ExpandableListView expadableListView = (ExpandableListView) view
 				.findViewById(R.id.favouritesExpandableListView);
 		FavouritesExpandableListAdapter adapter = new FavouritesExpandableListAdapter(
@@ -66,27 +88,27 @@ public class FavouritesFragment extends Fragment {
 	public boolean onContextItemSelected(MenuItem item) {
 		GeneralItem favouriteItem;
 		switch (item.getItemId()) {
-			case R.id.favourite_context_menu_view:
-				// TODO: Redirigir a la vista
-				favouriteItem = getFavouriteItem(item);
-				Toast.makeText(getActivity(), "View: " + favouriteItem.getName(),
-						Toast.LENGTH_SHORT).show();
-				return true;
-			case R.id.favourite_context_menu_edit:
-				// TODO: Redirigir a la vista de edicion
-				favouriteItem = getFavouriteItem(item);
-				Toast.makeText(getActivity(), "Edit: " + favouriteItem.getName(),
-						Toast.LENGTH_SHORT).show();
-				return true;
-			case R.id.favourite_context_menu_delete:
-				// TODO: Ejecutar la misma funcion para eliminarlo
-				favouriteItem = getFavouriteItem(item);
-				Toast.makeText(getActivity(), "Delete: " + favouriteItem.getName(),
-						Toast.LENGTH_SHORT).show();
-				return true;
-			default:
-				return super.onContextItemSelected(item);
-			}
+		case R.id.favourite_context_menu_view:
+			// TODO: Redirigir a la vista
+			favouriteItem = getFavouriteItem(item);
+			Toast.makeText(getActivity(), "View: " + favouriteItem.getName(),
+					Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.favourite_context_menu_edit:
+			// TODO: Redirigir a la vista de edicion
+			favouriteItem = getFavouriteItem(item);
+			Toast.makeText(getActivity(), "Edit: " + favouriteItem.getName(),
+					Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.favourite_context_menu_delete:
+			// TODO: Ejecutar la misma funcion para eliminarlo
+			favouriteItem = getFavouriteItem(item);
+			Toast.makeText(getActivity(), "Delete: " + favouriteItem.getName(),
+					Toast.LENGTH_SHORT).show();
+			return true;
+		default:
+			return super.onContextItemSelected(item);
+		}
 	}
 
 	private GeneralItem getFavouriteItem(MenuItem item) {
@@ -98,25 +120,67 @@ public class FavouritesFragment extends Fragment {
 				.getPackedPositionChild(info.packedPosition);
 		GroupFavouriteItems group = (GroupFavouriteItems) groups
 				.get(groupPosition);
-		GeneralItem favouriteItem = (GeneralItem) group
-				.getItem(childPosition);
+		GeneralItem favouriteItem = (GeneralItem) group.getItem(childPosition);
 		return favouriteItem;
 	}
 
 	public void createItemsGroups() {
-		GroupFavouriteItems poisGroup = new GroupFavouriteItems(
-				getString(R.string.favourite_pois_group),
-				R.drawable.ic_action_place);
-		GroupFavouriteItems toursGroup = new GroupFavouriteItems(
-				getString(R.string.favourite_tours_group),
-				R.drawable.ic_action_split);
-		// TODO: Realizar la llamada al servidor para cada uno
+		User user = ((GlobalContext) getActivity().getApplicationContext())
+				.getUser();
+		// TODO: Realizar la llamada al servidor para los Tours
 		for (int i = 0; i < 5; i++) {
-			poisGroup.add(new Poi("Prueba " + i, "Descripcion " + i));
 			toursGroup.add(new Tour("Prueba " + i, "Descripcion " + i));
 		}
+		getPoisFromServer(user.getId());
 		groups.put(POIS_GROUP_KEY, poisGroup);
 		groups.put(TOURS_GROUP_KEY, toursGroup);
 	}
 
+	private void getPoisFromServer(int userId) {
+
+		String url = getResources().getString(R.string.api_url)
+				+ "/pois/get_created_by_user.json?user_id=" + userId;
+		HttpAsyncTask task = new HttpAsyncTask();
+		task.execute(url);
+	}
+
+	private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+		private ApiInterface apiInterface = new ApiInterface();
+		private ApiResult api_result;
+
+		@Override
+		protected String doInBackground(String... urls) {
+			api_result = apiInterface.GET(urls[0]);
+			return api_result.getResult();
+		}
+
+		// onPostExecute displays the results of the AsyncTask.
+		@Override
+		protected void onPostExecute(String result) {
+			// Toast.makeText(getBaseContext(), "Received!",
+			// Toast.LENGTH_LONG).show();
+			Log.d("InputStream", result);
+			if (api_result.ok()) {
+				try {
+					JSONArray pois = new JSONArray(result);
+					for (int i = 0; i < pois.length(); i++) {
+						JSONObject poiJson = pois.getJSONObject(i);
+						Poi poi = Poi.fromJSON(poiJson);
+						poisGroup.add(poi);
+					}
+
+				} catch (JSONException e) {
+					// TODO: Handlear
+					e.printStackTrace();
+				} catch (ParseException e) {
+					// TODO: Handlear
+					e.printStackTrace();
+				}
+			} else {
+				// showConnectionError();
+				// TODO si no se pudieron obtener las categorias mostrar cartel
+				// para reintentar
+			}
+		}
+	}
 }
