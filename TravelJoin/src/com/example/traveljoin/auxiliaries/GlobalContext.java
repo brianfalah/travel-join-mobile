ea9ba85482309c6dffb985f2e87a06bed093cd99
hernan.pieroni@gmail.com <hernan.pieroni@gmail.com@704b2364-5aca-72fd-fb40-33b75dc59cb9>
2014-09-30 04:16:47 +0000

    GlobalContext: Se agregaron las categorias en el contexto global. Ahora solo se trae una unica vez al inicio y se persiste en el contexto global. Faltan las refactorizacion de los lugares donde se hacen llamados a la API para obtener las categorias para que directamente se lo soliciten al contexto.

--- TravelJoin/src/com/example/traveljoin/auxiliaries/GlobalContext.java
+++ TravelJoin/src/com/example/traveljoin/auxiliaries/GlobalContext.java
@@ -1,5 +1,9 @@
 package com.example.traveljoin.auxiliaries;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -7,10 +11,12 @@ import android.app.Application;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 import com.example.traveljoin.R;
 import com.example.traveljoin.models.ApiInterface;
 import com.example.traveljoin.models.ApiResult;
+import com.example.traveljoin.models.Category;
 import com.example.traveljoin.models.CustomTravelJoinException;
 import com.example.traveljoin.models.User;
 import com.facebook.Request;
@@ -21,8 +27,9 @@ import com.facebook.model.GraphUser;
 public class GlobalContext extends Application {
 
 	private User user;
-	public int taskExcutedInProgressDialog = 1;
-	
+	private List<Category> categories;
+	public int taskExcutedInProgressDialog = 2;
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -35,17 +42,26 @@ public class GlobalContext extends Application {
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
 	public void initializeContext(final FragmentActivity requesterActivity) {
-		
+
 		ProgressDialog progressDialog = new ProgressDialog(requesterActivity);
 		progressDialog.setTitle(getString(R.string.loading));
 		progressDialog.setMessage(getString(R.string.wait));
 		progressDialog.setCanceledOnTouchOutside(false);
 		progressDialog.setCancelable(false);
 		progressDialog.show();
-        
+
 		initializeUser(requesterActivity, progressDialog);
+		initializeCategories(requesterActivity, progressDialog);
 	}
 
 	private void initializeUser(final FragmentActivity requesterActivity,
@@ -63,8 +79,7 @@ public class GlobalContext extends Application {
 										.getString(R.string.api_url)
 										+ "/users/get_or_create";
 								User user = new User(facebookUser.getId(),
-										null,
-										facebookUser.getFirstName(),
+										null, facebookUser.getFirstName(),
 										facebookUser.getLastName());
 
 								GetOrCreateUserIfNotExistTask httpAsyncTask = new GetOrCreateUserIfNotExistTask(
@@ -79,13 +94,14 @@ public class GlobalContext extends Application {
 					}
 				});
 		request.executeAsync();
-
 	}
-	
+
 	private void initializeCategories(FragmentActivity requesterActivity,
 			ProgressDialog progressDialog) {
-		// TODO Auto-generated method stub
-		
+		String url = getResources().getString(R.string.api_url)
+				+ "/categories/index.json";
+		GetCategoriesTask httpAsyncTask = new GetCategoriesTask(progressDialog);
+		httpAsyncTask.execute(url);
 	}
 
 	private class GetOrCreateUserIfNotExistTask extends
@@ -127,11 +143,82 @@ public class GlobalContext extends Application {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} finally {
-				if (progressDialog.isShowing()) {
-						progressDialog.dismiss();
+				taskExcutedInProgressDialog -= 1;
+				if (progressDialog.isShowing()
+						&& taskExcutedInProgressDialog == 0) {
+					progressDialog.dismiss();
 				}
 			}
 		}
 	}
 
+	private class GetCategoriesTask extends AsyncTask<String, Void, String> {
+		private ApiInterface apiInterface = new ApiInterface();
+		private ApiResult apiResult;
+		private ProgressDialog progressDialog;
+
+		public GetCategoriesTask(ProgressDialog progressDialog) {
+			this.progressDialog = progressDialog;
+		}
+
+		@Override
+		protected String doInBackground(String... urls) {
+			apiResult = apiInterface.GET(urls[0]);
+			return apiResult.getResult();
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Log.d("InputStream", result);
+			try {
+				if (apiResult.ok()) {
+					List<Category> categories = getCategoriesFromJSON(new JSONArray(
+							result));
+					GlobalContext globalContext = (GlobalContext) getApplicationContext();
+					globalContext.setCategories(categories);
+				}
+			} catch (JSONException e) {
+				// TODO
+				showExceptionError(e);
+
+			} finally {
+				taskExcutedInProgressDialog -= 1;
+				if (progressDialog.isShowing()
+						&& taskExcutedInProgressDialog == 0) {
+					progressDialog.dismiss();
+				}
+			}
+
+		}
+
+		private List<Category> getCategoriesFromJSON(JSONArray categoriesJSON)
+				throws JSONException {
+			List<Category> categories = new ArrayList<Category>();
+
+			for (int i = 0; i < categoriesJSON.length(); i++) {
+				JSONObject categoryJSON = categoriesJSON.getJSONObject(i);
+				Category category = new Category(categoryJSON.getInt("id"),
+						categoryJSON.getString("name"));
+				categories.add(category);
+			}
+
+			return categories;
+		}
+	}
+
+	// TODO Refactor de showConnectionError y showExceptionError a un Error
+	// Handler si es que siempre se hace lo mismo con las excepciones. Buscar
+	// esos metodos en todo el proyecto
+	public void showConnectionError() {
+		CustomTravelJoinException exception = new CustomTravelJoinException();
+		exception.alertConnectionProblem(getApplicationContext());
+		// e.printStackTrace();
+	}
+
+	public void showExceptionError(Exception e) {
+		CustomTravelJoinException exception = new CustomTravelJoinException(
+				e.getMessage());
+		exception.alertExceptionMessage(getApplicationContext());
+		e.printStackTrace();
+	}
 }
