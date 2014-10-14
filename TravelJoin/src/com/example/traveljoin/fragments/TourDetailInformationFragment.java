bb64bd48b49d072875bb175fc1f9e7f4da4e8ffd
hernan.pieroni@gmail.com <hernan.pieroni@gmail.com@704b2364-5aca-72fd-fb40-33b75dc59cb9>
2014-10-14 17:43:57 +0000

    Se agrega el administrador al circuito

--- TravelJoin/src/com/example/traveljoin/fragments/TourDetailInformationFragment.java
+++ TravelJoin/src/com/example/traveljoin/fragments/TourDetailInformationFragment.java
@@ -2,6 +2,10 @@ package com.example.traveljoin.fragments;
 
 import com.example.traveljoin.R;
 import com.example.traveljoin.activities.TourDetailsActivity;
+import com.example.traveljoin.auxiliaries.GlobalContext;
+import com.example.traveljoin.models.User;
+import com.facebook.widget.ProfilePictureView;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -26,8 +30,22 @@ public class TourDetailInformationFragment  extends Fragment {
 		tvName = (TextView) view.findViewById(R.id.TourName);
 		tvDesc = (TextView) view.findViewById(R.id.TourDescription);
 		setFields();       
-
+		initializeOwnerInformation(view);
+		
 		return view;
+	}
+	
+	private void initializeOwnerInformation(View view) {
+		ProfilePictureView  profilePictureView = (ProfilePictureView) view
+				.findViewById(R.id.selection_profile_pic);
+		profilePictureView.setCropped(true);
+		TextView userOwnerNameView = (TextView) view.findViewById(R.id.selection_owner_name);
+
+		GlobalContext globalContext = (GlobalContext) getActivity()
+				.getApplicationContext();
+		User owner = globalContext.getUser();
+		profilePictureView.setProfileId(owner.getFacebookId());
+		userOwnerNameView.setText(owner.getFullName());
 	}
 	
 	public void setFields(){
