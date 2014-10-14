3e0f5304465974eb89984e86a3b9897d51ddfedb
hernan.pieroni@gmail.com <hernan.pieroni@gmail.com@704b2364-5aca-72fd-fb40-33b75dc59cb9>
2014-10-14 07:01:24 +0000

    Se agrego la seleccion de tours desde el formulario de grupos. Falta testear una vez que el alta de un tour este terminado.

--- trunk/TravelJoin/src/com/example/traveljoin/fragments/GroupFormToursFragment.java
+++ trunk/TravelJoin/src/com/example/traveljoin/fragments/GroupFormToursFragment.java
@@ -3,6 +3,8 @@ package com.example.traveljoin.fragments;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.ContextMenu;
@@ -14,6 +16,7 @@ import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 
 import com.example.traveljoin.R;
+import com.example.traveljoin.activities.ToursSelectorActivity;
 import com.example.traveljoin.adapters.GeneralItemListAdapter;
 import com.example.traveljoin.models.GeneralItem;
 import com.example.traveljoin.models.Tour;
@@ -21,46 +24,43 @@ import com.example.traveljoin.models.Tour;
 public class GroupFormToursFragment extends ListFragment {
 	private ArrayList<GeneralItem> tours;
 	private GeneralItemListAdapter toursAdapter;
-	
+	private static final int ADD_TOURS_REQUEST = 1;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setHasOptionsMenu(true);
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 	}
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		tours = new ArrayList<GeneralItem>();
-
-		tours.add(new Tour("Tour 1", "Descripcion 1"));
-		tours.add(new Tour("Tour 2", "Descripcion 2"));
-		tours.add(new Tour("Tour 3", "Descripcion 3"));
-
-		toursAdapter = new GeneralItemListAdapter(
-				getActivity(), tours);
+		toursAdapter = new GeneralItemListAdapter(getActivity(), tours);
 		setListAdapter(toursAdapter);
 		registerForContextMenu(getListView());
 	}
-	
+
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-	    inflater.inflate(R.menu.only_add_item_action, menu);
-	    super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.only_add_item_action, menu);
+		super.onCreateOptionsMenu(menu, inflater);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    switch (item.getItemId()) {
-	        case R.id.item_add:
-	        	tours.add(new Tour("Tour Nuevo", "Descripcion nueva"));
-	        	toursAdapter.notifyDataSetChanged();
-	            return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		switch (item.getItemId()) {
+		case R.id.item_add:
+			Intent intent = new Intent(getActivity(),
+					ToursSelectorActivity.class);
+			intent.putExtra("alreadySelectedTours", tours);
+			startActivityForResult(intent, ADD_TOURS_REQUEST);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
-	
+
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
@@ -76,7 +76,7 @@ public class GroupFormToursFragment extends ListFragment {
 		case R.id.context_menu_delete:
 			selectedTour = getTourItem(item);
 			tours.remove(selectedTour);
-        	toursAdapter.notifyDataSetChanged();
+			toursAdapter.notifyDataSetChanged();
 			return true;
 		default:
 			return super.onContextItemSelected(item);
@@ -84,15 +84,35 @@ public class GroupFormToursFragment extends ListFragment {
 	}
 
 	private Tour getTourItem(MenuItem item) {
-		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+				.getMenuInfo();
 		Tour tour = (Tour) tours.get(info.position);
 		return tour;
 	}
-	
+
 	public ArrayList<Tour> getTours() {
 		ArrayList<Tour> auxTours = new ArrayList<Tour>();
-		auxTours.addAll((Collection<? extends Tour>) tours);		
+		auxTours.addAll((Collection<? extends Tour>) tours);
 		return auxTours;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case ADD_TOURS_REQUEST:
+			switch (resultCode) {
+			case Activity.RESULT_OK:
+				Bundle bundle = data.getExtras();
+				tours.clear();
+				tours.addAll((ArrayList<GeneralItem>) bundle.get("newSelectedTours"));
+				toursAdapter.notifyDataSetChanged();
+				break;
+			case Activity.RESULT_CANCELED:
+				break;
+			}
+			break;
+		}
 	}
 
 }
