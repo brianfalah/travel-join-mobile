adbd872e54d6a2dfb907e005c2a3f59f1921160b
hernan.pieroni@gmail.com <hernan.pieroni@gmail.com@704b2364-5aca-72fd-fb40-33b75dc59cb9>
2014-10-14 06:09:26 +0000

    Se agrego que ante un tap sobre un tour se dirija a la vista del tour

--- TravelJoin/src/com/example/traveljoin/activities/ToursMainActivity.java
+++ TravelJoin/src/com/example/traveljoin/activities/ToursMainActivity.java
@@ -23,9 +23,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.SearchView.OnQueryTextListener;
 
 import com.example.traveljoin.R;
@@ -48,15 +50,23 @@ public class ToursMainActivity extends Activity implements OnQueryTextListener {
 	private GeneralItemListAdapter adapter;
 	private ArrayList<GeneralItem> tours;
 	private static final int EDIT_TOUR_REQUEST = 1;
-	//para el asynctask
+	// para el asynctask
 	protected static final int GET_TOURS_METHOD = 1;
 	protected static final int DELETE_TOUR_METHOD = 2;
-	
+
+	OnItemClickListener tourItemClickListener = new OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			startTourDetailActivity((Tour) adapter.getItem(position));
+		}
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list);
-		
+
 		initializeUser();
 		actionBar = getActionBar();
 		actionBar.setSubtitle(R.string.tours);
@@ -70,7 +80,7 @@ public class ToursMainActivity extends Activity implements OnQueryTextListener {
 		listView.setTextFilterEnabled(true);
 		registerForContextMenu(listView);
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.tours_activity_actions, menu);
@@ -114,15 +124,15 @@ public class ToursMainActivity extends Activity implements OnQueryTextListener {
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v,
 			ContextMenuInfo menuInfo) {
-		super.onCreateContextMenu(menu, v, menuInfo);		
+		super.onCreateContextMenu(menu, v, menuInfo);
 		getMenuInflater().inflate(R.menu.tour_list_item_context_menu, menu);
-		
+
 		AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
-	    Tour tour = (Tour) listView.getItemAtPosition(info.position);
-	    if (!tour.getUserId().equals(user.getId())){
-	    	menu.removeItem(R.id.poi_context_menu_edit);
-	    	menu.removeItem(R.id.poi_context_menu_delete);
-	    }
+		Tour tour = (Tour) listView.getItemAtPosition(info.position);
+		if (!tour.getUserId().equals(user.getId())) {
+			menu.removeItem(R.id.poi_context_menu_edit);
+			menu.removeItem(R.id.poi_context_menu_delete);
+		}
 	}
 
 	@Override
@@ -130,66 +140,86 @@ public class ToursMainActivity extends Activity implements OnQueryTextListener {
 		final Tour selectedTour = getTourItem(item);
 		switch (item.getItemId()) {
 		case R.id.tour_context_menu_view:
-			Intent intent = new Intent(this, TourDetailsActivity.class);
-    		intent.putExtra("tour", selectedTour); //le pasamos el punto a la activity
-    		startActivity(intent);	
+			startTourDetailActivity(selectedTour);
 			return true;
-		case R.id.tour_context_menu_edit:			
+		case R.id.tour_context_menu_edit:
 			Intent intent_edit = new Intent(this, TourFormActivity.class);
-			intent_edit.putExtra("tour", selectedTour); //le pasamos el punto al form
-			//va al form para editarlo
+			intent_edit.putExtra("tour", selectedTour); // le pasamos el punto
+														// al form
+			// va al form para editarlo
 			startActivityForResult(intent_edit, EDIT_TOUR_REQUEST);
 			return true;
 		case R.id.tour_context_menu_delete:
-			AlertDialog.Builder dialog = new AlertDialog.Builder(ToursMainActivity.this);
+			AlertDialog.Builder dialog = new AlertDialog.Builder(
+					ToursMainActivity.this);
 			dialog.setTitle("Borrar punto")
-	        .setMessage("¿Esta seguro de que desea borrar este circuito?")
-	        .setPositiveButton("Si", new DialogInterface.OnClickListener() {
-	            public void onClick(DialogInterface dialog, int which) {
-	            	progress = ProgressDialog.show(ToursMainActivity.this, "Cargando",
-	                	    "Por favor espere...", true);
-	            	String url = getResources().getString(R.string.api_url) + "/tours/destroy";
-	    	        HttpAsyncTask httpAsyncTask = new HttpAsyncTask(DELETE_TOUR_METHOD, selectedTour); 
-	    	        httpAsyncTask.execute(url);
-	    	        //sigue en HttpAsyncTask en doInBackground en DELETE_POI_METHOD
-	            	
-	            }
-	         })
-	         .setNegativeButton("No", new DialogInterface.OnClickListener() {
-	            public void onClick(DialogInterface dialog, int which) { 
-	                // do nothing
-	            }
-	         })
-			.setIcon(android.R.drawable.ic_dialog_alert)
-	         .show();			
+					.setMessage(
+							"¿Está seguro de que desea borrar este circuito?")
+					.setPositiveButton("Si",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int which) {
+									progress = ProgressDialog.show(
+											ToursMainActivity.this,
+											getString(R.string.loading),
+											getString(R.string.wait), true);
+									String url = getResources().getString(
+											R.string.api_url)
+											+ "/tours/destroy";
+									HttpAsyncTask httpAsyncTask = new HttpAsyncTask(
+											DELETE_TOUR_METHOD, selectedTour);
+									httpAsyncTask.execute(url);
+									// sigue en HttpAsyncTask en doInBackground
+									// en DELETE_POI_METHOD
+
+								}
+							})
+					.setNegativeButton("No",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int which) {
+									// do nothing
+								}
+							}).setIcon(android.R.drawable.ic_dialog_alert)
+					.show();
 			return true;
 		default:
 			return super.onContextItemSelected(item);
 		}
 	}
-	
+
+	private void startTourDetailActivity(final Tour selectedTour) {
+		Intent intent = new Intent(this, TourDetailsActivity.class);
+		intent.putExtra("tour", selectedTour); // le pasamos el punto a la
+												// activity
+		startActivity(intent);
+	}
+
 	private Tour getTourItem(MenuItem item) {
-		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+				.getMenuInfo();
 		Tour tour = (Tour) tours.get(info.position);
 		return tour;
 	}
-	
-    /*Cuando vuelve de un activity empezado con un startActivityForResult viene aca*/
-    @Override
-    protected void onActivityResult(
-    		int requestCode, int resultCode, Intent data) {
-    	// Decide what to do based on the original request code
-    	switch (requestCode) {
-	    	case EDIT_TOUR_REQUEST :
-	    		switch (resultCode) {
-		    		case Activity.RESULT_OK :
-		    			getToursFromServer();
-		    		break;
-	    		}
-	    	break;	    	
-	    }
 
-    }
+	/*
+	 * Cuando vuelve de un activity empezado con un startActivityForResult viene
+	 * aca
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// Decide what to do based on the original request code
+		switch (requestCode) {
+		case EDIT_TOUR_REQUEST:
+			switch (resultCode) {
+			case Activity.RESULT_OK:
+				getToursFromServer();
+				break;
+			}
+			break;
+		}
+
+	}
 
 	private void getToursFromServer() {
 		progress = ProgressDialog.show(this, getString(R.string.loading),
@@ -202,76 +232,80 @@ public class ToursMainActivity extends Activity implements OnQueryTextListener {
 
 	private class HttpAsyncTask extends AsyncTask<String, Void, String> {
 		private ApiInterface apiInterface = new ApiInterface();
-    	private Integer from_method;
-    	private Object object_to_send;
-    	private ApiResult api_result;
-    	
-    	//contructor para setearle info extra
-    	public HttpAsyncTask(Integer from_method, Object object_to_send) {
+		private Integer from_method;
+		private Object object_to_send;
+		private ApiResult api_result;
+
+		// contructor para setearle info extra
+		public HttpAsyncTask(Integer from_method, Object object_to_send) {
 			this.from_method = from_method;
-			this.object_to_send = object_to_send;  
+			this.object_to_send = object_to_send;
 		}
 
 		@Override
 		protected String doInBackground(String... urls) {
-        	//despues de cualquiera de estos metodo vuelve al postexecute de aca
-        	switch (this.from_method) {
-	        	case GET_TOURS_METHOD :
-	        		api_result = apiInterface.GET(urls[0]);
-	        	break;
-	        	case DELETE_TOUR_METHOD :
-	        		api_result = apiInterface.POST(urls[0], object_to_send, "delete");
-	        	break;
-        	}
-        	
-        	return api_result.getResult();           				
+			// despues de cualquiera de estos metodo vuelve al postexecute de
+			// aca
+			switch (this.from_method) {
+			case GET_TOURS_METHOD:
+				api_result = apiInterface.GET(urls[0]);
+				break;
+			case DELETE_TOUR_METHOD:
+				api_result = apiInterface.POST(urls[0], object_to_send,
+						"delete");
+				break;
+			}
+
+			return api_result.getResult();
 		}
 
 		// onPostExecute displays the results of the AsyncTask.
 		@Override
 		protected void onPostExecute(String result) {
 			Log.d("InputStream", result);
-        	switch (this.from_method) {   
-	        	case GET_TOURS_METHOD :
-	    			if (api_result.ok()) {
-	    				try {
-	    					tours.clear();
-	    					JSONArray toursJson = new JSONArray(result);
-	    					for (int i = 0; i < toursJson.length(); i++) {
-	    						JSONObject tourJson = toursJson.getJSONObject(i);
-	    						Tour tour = Tour.fromJSON(tourJson);	    						
-	    						tours.add(tour);
-	    					}
-	    					adapter.notifyDataSetChanged();
-	    					progress.dismiss();
+			switch (this.from_method) {
+			case GET_TOURS_METHOD:
+				if (api_result.ok()) {
+					try {
+						tours.clear();
+						JSONArray toursJson = new JSONArray(result);
+						for (int i = 0; i < toursJson.length(); i++) {
+							JSONObject tourJson = toursJson.getJSONObject(i);
+							Tour tour = Tour.fromJSON(tourJson);
+							tours.add(tour);
+						}
+						adapter.notifyDataSetChanged();
+						progress.dismiss();
 
-	    				} catch (JSONException e) {
-	    					// TODO: Handlear
-	    					progress.dismiss();
-	    					e.printStackTrace();
-	    				} catch (ParseException e) {
-	    					// TODO: Handlear
-	    					e.printStackTrace();
-	    				}
-	    			} else {
-	    				// showConnectionError();
-	    				// TODO si no se pudieron obtener las categorias mostrar cartel
-	    				// para reintentar
-	    			}
-			    break;
-	        	case DELETE_TOUR_METHOD :
-	        		progress.dismiss(); 
-					if (api_result.ok())												
-						getToursFromServer();
-					else{
-						CustomTravelJoinException exception = new CustomTravelJoinException("No se ha podido borrar el circuito correctamente.");
-						exception.alertExceptionMessage(ToursMainActivity.this);
-					}								
-			    break;	
-	    	}        	
+					} catch (JSONException e) {
+						// TODO: Handlear
+						progress.dismiss();
+						e.printStackTrace();
+					} catch (ParseException e) {
+						// TODO: Handlear
+						e.printStackTrace();
+					}
+				} else {
+					// showConnectionError();
+					// TODO si no se pudieron obtener las categorias mostrar
+					// cartel
+					// para reintentar
+				}
+				break;
+			case DELETE_TOUR_METHOD:
+				progress.dismiss();
+				if (api_result.ok())
+					getToursFromServer();
+				else {
+					CustomTravelJoinException exception = new CustomTravelJoinException(
+							"No se ha podido borrar el circuito correctamente.");
+					exception.alertExceptionMessage(ToursMainActivity.this);
+				}
+				break;
+			}
 		}
 	}
-	
+
 	private void initializeUser() {
 		GlobalContext globalContext = (GlobalContext) getApplicationContext();
 		user = globalContext.getUser();
