df46d51e2f42260617cf86d0bcfcaa69d2d9877e
briancf@gmail.com <briancf@gmail.com@704b2364-5aca-72fd-fb40-33b75dc59cb9>
2014-10-14 01:13:49 +0000

    terminado form de POIS

--- trunk/TravelJoin/src/com/example/traveljoin/fragments/PoiEventsFragment.java
+++ trunk/TravelJoin/src/com/example/traveljoin/fragments/PoiEventsFragment.java
@@ -19,6 +19,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.Adapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,6 +29,7 @@ public class PoiEventsFragment extends ListFragment {
 
 	PoiDetailsActivity activity;
 	private ArrayList<GeneralItem> events;
+	GeneralItemListAdapter adapter;
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
@@ -37,61 +39,67 @@ public class PoiEventsFragment extends ListFragment {
 		events = new ArrayList<GeneralItem>();
 		events.addAll(activity.poi.getPoiEvents());
 
-		GeneralItemListAdapter adapter = new GeneralItemListAdapter(
+		adapter = new GeneralItemListAdapter(
 				getActivity(), events);
 		setListAdapter(adapter);
-		registerForContextMenu(getListView());
+//		registerForContextMenu(getListView());
 	}
 
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
-		super.onCreateContextMenu(menu, v, menuInfo);
-		MenuInflater inflater = getActivity().getMenuInflater();
-		inflater.inflate(R.menu.group_list_item_context_menu, menu);
-	}
-
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		Group selectedGroup; 
-		switch (item.getItemId()) {
-		case R.id.group_context_menu_view:
-			// TODO: Redirigir a la vista del Grupo de vista
-			selectedGroup = getGroupItem(item);
-			Toast.makeText(getActivity(),
-					"View : " + selectedGroup.getName(),
-					Toast.LENGTH_SHORT).show();
-			return true;
-		case R.id.group_context_menu_edit:
-			// TODO: Redirigir a la vista del Grupo de edicion
-			selectedGroup = getGroupItem(item);
-			Toast.makeText(getActivity(),
-					"Edit : " + selectedGroup.getName(),
-					Toast.LENGTH_SHORT).show();
-			return true;
-		case R.id.group_context_menu_delete:
-			// TODO: Ejecutar la misma funcion para eliminar un Grupo
-			selectedGroup = getGroupItem(item);
-			Toast.makeText(getActivity(),
-					"Delete : " + selectedGroup.getName(),
-					Toast.LENGTH_SHORT).show();
-			return true;
-		default:
-			return super.onContextItemSelected(item);
-		}
-	}
-
-	
-	private Group getGroupItem(MenuItem item) {
-		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-		Group group = (Group) events.get(info.position);
-		return group;
-	}
+//	@Override
+//	public void onCreateContextMenu(ContextMenu menu, View v,
+//			ContextMenuInfo menuInfo) {
+//		super.onCreateContextMenu(menu, v, menuInfo);
+//		MenuInflater inflater = getActivity().getMenuInflater();
+//		inflater.inflate(R.menu.group_list_item_context_menu, menu);
+//	}
+//
+//	@Override
+//	public boolean onContextItemSelected(MenuItem item) {
+//		Group selectedGroup; 
+//		switch (item.getItemId()) {
+//		case R.id.group_context_menu_view:
+//			// TODO: Redirigir a la vista del Grupo de vista
+//			selectedGroup = getGroupItem(item);
+//			Toast.makeText(getActivity(),
+//					"View : " + selectedGroup.getName(),
+//					Toast.LENGTH_SHORT).show();
+//			return true;
+//		case R.id.group_context_menu_edit:
+//			// TODO: Redirigir a la vista del Grupo de edicion
+//			selectedGroup = getGroupItem(item);
+//			Toast.makeText(getActivity(),
+//					"Edit : " + selectedGroup.getName(),
+//					Toast.LENGTH_SHORT).show();
+//			return true;
+//		case R.id.group_context_menu_delete:
+//			// TODO: Ejecutar la misma funcion para eliminar un Grupo
+//			selectedGroup = getGroupItem(item);
+//			Toast.makeText(getActivity(),
+//					"Delete : " + selectedGroup.getName(),
+//					Toast.LENGTH_SHORT).show();
+//			return true;
+//		default:
+//			return super.onContextItemSelected(item);
+//		}
+//	}
+//
+//	
+//	private Group getGroupItem(MenuItem item) {
+//		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+//		Group group = (Group) events.get(info.position);
+//		return group;
+//	}
 
 	@Override
 	public void onListItemClick(ListView listView, View view, int position,
 			long id) {
 		// TODO: Redirigir a la vista del POI de vista
+	}
+	
+	public void refreshList(){
+		events.clear();
+		events.addAll(activity.poi.getPoiEvents());
+		adapter.notifyDataSetChanged();
 	}
 	
 }
