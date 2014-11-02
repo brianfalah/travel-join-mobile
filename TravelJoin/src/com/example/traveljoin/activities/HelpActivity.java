de426b12c3b2c090d4355e3bc49f2d904da63874
pablo.biscione@gmail.com <pablo.biscione@gmail.com@704b2364-5aca-72fd-fb40-33b75dc59cb9>
2014-11-02 00:59:33 +0000

    Help

---
+++ TravelJoin/src/com/example/traveljoin/activities/HelpActivity.java
@@ -0,0 +1,14 @@
+package com.example.traveljoin.activities;
+
+import com.example.traveljoin.R;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+public class HelpActivity extends ActionBarActivity {
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help);	
+
+	}}
