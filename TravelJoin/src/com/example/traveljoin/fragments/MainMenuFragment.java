6fdad794ed5281dc0a1d40076202bd43ffa99cef
hernan.pieroni@gmail.com <hernan.pieroni@gmail.com@704b2364-5aca-72fd-fb40-33b75dc59cb9>
2014-11-04 23:14:51 +0000

    Agregar menú de ayuda al panel lateral

--- TravelJoin/src/com/example/traveljoin/fragments/MainMenuFragment.java
+++ TravelJoin/src/com/example/traveljoin/fragments/MainMenuFragment.java
@@ -1,4 +1,5 @@
 package com.example.traveljoin.fragments;
+
 import java.util.ArrayList;
 
 import android.content.Context;
@@ -16,6 +17,7 @@ import android.widget.TextView;
 import com.example.traveljoin.R;
 import com.example.traveljoin.activities.AugmentedRealityActivity;
 import com.example.traveljoin.activities.GroupsMainActivity;
+import com.example.traveljoin.activities.HelpActivity;
 import com.example.traveljoin.activities.MapActivity;
 import com.example.traveljoin.activities.PoisMainActivity;
 import com.example.traveljoin.activities.ToursMainActivity;
@@ -24,40 +26,50 @@ import com.example.traveljoin.models.Poi;
 
 public class MainMenuFragment extends ListFragment {
 
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.main_menu, null);
 	}
 
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		MainMenuItemAdapter adapter = new MainMenuItemAdapter(getActivity());
-		adapter.add(new MainMenuItem(getString(R.string.profile), R.drawable.ic_action_person));
-		adapter.add(new MainMenuItem(getString(R.string.pois), R.drawable.ic_action_place));
-		adapter.add(new MainMenuItem(getString(R.string.tours), R.drawable.ic_action_split));
-		adapter.add(new MainMenuItem(getString(R.string.groups), R.drawable.ic_action_group));
-		adapter.add(new MainMenuItem(getString(R.string.augmented_reality), R.drawable.ic_action_full_screen));
+		adapter.add(new MainMenuItem(getString(R.string.profile),
+				R.drawable.ic_action_person));
+		adapter.add(new MainMenuItem(getString(R.string.pois),
+				R.drawable.ic_action_place));
+		adapter.add(new MainMenuItem(getString(R.string.tours),
+				R.drawable.ic_action_split));
+		adapter.add(new MainMenuItem(getString(R.string.groups),
+				R.drawable.ic_action_group));
+		adapter.add(new MainMenuItem(getString(R.string.augmented_reality),
+				R.drawable.ic_action_full_screen));
+		adapter.add(new MainMenuItem(getString(R.string.help),
+				R.drawable.ic_action_help_dark));
 		setListAdapter(adapter);
 	}
 
 	private class MainMenuItem {
 		public String tag;
 		public int iconRes;
-		
+
 		public MainMenuItem(String tag, int iconRes) {
-			this.tag = tag; 
+			this.tag = tag;
 			this.iconRes = iconRes;
 		}
 	}
-	
+
 	@Override
-    public void onListItemClick(ListView l, View v, int position, long id){
+	public void onListItemClick(ListView l, View v, int position, long id) {
 		Class<?> cls = null;
-		String title = ((MainMenuItem) l.getItemAtPosition(position)).tag;		
-		
+		String title = ((MainMenuItem) l.getItemAtPosition(position)).tag;
+
 		Bundle b = new Bundle();
 		if (title.equals(getString(R.string.profile))) {
-			GlobalContext globalContext = (GlobalContext) getActivity().getApplicationContext();
-			globalContext.refreshUserAndLaunchUserProfile((MapActivity) getActivity());
+			GlobalContext globalContext = (GlobalContext) getActivity()
+					.getApplicationContext();
+			globalContext
+					.refreshUserAndLaunchUserProfile((MapActivity) getActivity());
 			return;
 		} else if (title.equals(getString(R.string.pois))) {
 			cls = PoisMainActivity.class;
@@ -65,16 +77,19 @@ public class MainMenuFragment extends ListFragment {
 			cls = ToursMainActivity.class;
 		} else if (title.equals(getString(R.string.groups))) {
 			cls = GroupsMainActivity.class;
-		}else if (title.equals(getString(R.string.augmented_reality))) {
+		} else if (title.equals(getString(R.string.augmented_reality))) {
 			cls = AugmentedRealityActivity.class;
-			ArrayList<Poi> pois = new ArrayList<Poi>(MapActivity.markerPoiMap.values());			
+			ArrayList<Poi> pois = new ArrayList<Poi>(
+					MapActivity.markerPoiMap.values());
 			b.putSerializable("pois", pois);
-		}	
-        
-        Intent intent = new Intent(getActivity(), cls);
-        intent.putExtra("b", b); //le pasamos el punto al form
-        startActivity(intent);
-    }  
+		} else if (title.equals(getString(R.string.help))) {
+			cls = HelpActivity.class;
+		}
+
+		Intent intent = new Intent(getActivity(), cls);
+		intent.putExtra("b", b); // le pasamos el punto al form
+		startActivity(intent);
+	}
 
 	private class MainMenuItemAdapter extends ArrayAdapter<MainMenuItem> {
 
@@ -84,16 +99,20 @@ public class MainMenuFragment extends ListFragment {
 
 		public View getView(int position, View convertView, ViewGroup parent) {
 			MainMenuItem item = getItem(position);
-			
+
 			if (convertView == null) {
-				convertView = LayoutInflater.from(getContext()).inflate(R.layout.main_menu_item, null);
+				convertView = LayoutInflater.from(getContext()).inflate(
+						R.layout.main_menu_item, null);
 			}
-			
-			convertView.setBackgroundColor(getResources().getColor(R.color.common_signin_btn_dark_text_pressed));
-			
-			ImageView icon = (ImageView) convertView.findViewById(R.id.row_icon);
+
+			convertView.setBackgroundColor(getResources().getColor(
+					R.color.common_signin_btn_dark_text_pressed));
+
+			ImageView icon = (ImageView) convertView
+					.findViewById(R.id.row_icon);
 			icon.setImageResource(item.iconRes);
-			TextView title = (TextView) convertView.findViewById(R.id.row_title);
+			TextView title = (TextView) convertView
+					.findViewById(R.id.row_title);
 			title.setText(item.tag);
 
 			return convertView;
